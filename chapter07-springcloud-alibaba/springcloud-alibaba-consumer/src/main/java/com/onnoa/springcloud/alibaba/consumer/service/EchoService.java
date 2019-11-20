8a230dc8e741a60fe88aa1f1c7f646134ff492ee
onnoa <onnoaheng@163.com>
2019-11-21 01:19:28 +0800

    apache dubbo demo

--- chapter07-springcloud-alibaba/springcloud-alibaba-consumer/src/main/java/com/onnoa/springcloud/alibaba/consumer/service/EchoService.java
+++ chapter07-springcloud-alibaba/springcloud-alibaba-consumer/src/main/java/com/onnoa/springcloud/alibaba/consumer/service/EchoService.java
@@ -1,10 +1,11 @@
 package com.onnoa.springcloud.alibaba.consumer.service;
 
+import com.onnoa.springcloud.alibaba.consumer.service.fallback.EchoServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "service-provider")
+@FeignClient(value = "service-provider",fallback = EchoServiceFallback.class)
 public interface EchoService {
 
     @GetMapping(value = "/echo/{string}")
