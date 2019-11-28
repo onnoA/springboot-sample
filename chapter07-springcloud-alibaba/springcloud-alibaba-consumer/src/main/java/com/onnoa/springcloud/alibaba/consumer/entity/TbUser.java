c3673468abcbabd4e2e253a13253304ab5323a5a
onnoA <onnoaheng@163.com>
2019-11-28 18:03:27 +0800

    邮件发送

--- chapter07-springcloud-alibaba/springcloud-alibaba-consumer/src/main/java/com/onnoa/springcloud/alibaba/consumer/entity/TbUser.java
+++ chapter07-springcloud-alibaba/springcloud-alibaba-consumer/src/main/java/com/onnoa/springcloud/alibaba/consumer/entity/TbUser.java
@@ -3,19 +3,34 @@ package com.onnoa.springcloud.alibaba.consumer.entity;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description:
  * @Author: onnoA
  * @Date: 2019/11/28 17:18
  */
+@TableName("tb_user")
 @Data
 public class TbUser implements Serializable {
 
-    private static final long serialVersionUID = -8048950189519392128L;
+    private static final long serialVersionUID = 4978928916244092281L;
+    private long id;
 
-    private Integer id;
     private String username;
+
+    private String password;
+
+    private String phone;
+
     private String email;
+
+    private Date created;
+
+    private Date updated;
+
+    private int deleted;
+
+    private int version;
 
 }
